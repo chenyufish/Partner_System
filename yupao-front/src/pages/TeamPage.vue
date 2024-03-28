4eb403a1f23a00687d44e544c2d6585f2c6389ce
fishman <cy13692810010@163.com>
2024-03-29 01:12:35 +0800

    开始做前端数据接收暂时，初步完成队伍页面搜索展示和加入功能

--- yupao-front/src/pages/TeamPage.vue
+++ yupao-front/src/pages/TeamPage.vue
@@ -1,18 +1,32 @@
 <template>
-  队伍
+<div id="teamPage">
+  <van-button type="primary" @click="doJoinTeam">加入队伍</van-button>
+  <team-card-list :teamList="teamList" />
+</div>
 </template>
 
+<script setup lang="ts">
+import {useRouter} from "vue-router";
+import {onMounted, ref} from "vue";
+import myAxios from "../plugins/myAxios.js";
+import {showFailToast} from "vant";
+import TeamCardList from "../components/TeamCardList.vue";
 
-<script lang="ts">
-import {defineComponent} from 'vue'
+const router=useRouter();
+const doJoinTeam=()=>{
+  router.push({
+    path:"/team/add"
+  })
+}
 
-export default defineComponent({
-  name: "Team"
+const teamList=ref([]);
+
+onMounted(async ()=>{
+  const res=await myAxios.get("/team/list");
+  if(res?.code===0) teamList.value=res.data;
+  else showFailToast("加载队伍失败，请刷新！")
 })
-
 </script>
-
-
 
 <style scoped>
 
