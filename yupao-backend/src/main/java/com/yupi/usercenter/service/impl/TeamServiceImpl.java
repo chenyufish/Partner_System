ede2525c7272ebf0c4dcc58fb2239cba52205d5c
fishman <cy13692810010@163.com>
2024-03-29 23:58:54 +0800

    前端增加搜索队伍，加入队伍，解散队伍

--- yupao-backend/src/main/java/com/yupi/usercenter/service/impl/TeamServiceImpl.java
+++ yupao-backend/src/main/java/com/yupi/usercenter/service/impl/TeamServiceImpl.java
@@ -37,8 +37,6 @@ import java.util.concurrent.TimeUnit;
 /**
  * 队伍服务实现类
  *
- * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
- * @from <a href="https://yupi.icu">编程导航知识星球</a>
  */
 @Service
 public class TeamServiceImpl extends ServiceImpl<TeamMapper, Team>
